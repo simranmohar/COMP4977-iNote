6e4550721f31490e1c908b496efe312c5fae053f
simranmohar <simranmohar@gmail.com>
2022-10-11 19:09:11 -0700

    Initial Commit

---
+++ COMP4977_Project/COMP4977_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  COMP4977_ProjectApp.swift
+//  COMP4977_Project
+//
+//  Created by Simran Mohar on 2022-10-11.
+//
+
+import SwiftUI
+
+@main
+struct COMP4977_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
